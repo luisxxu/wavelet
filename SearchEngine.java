084534c583e2ca0851df934ea734b8be49f531a0
luisxxu <lxxu@ucsd.edu>
2023-01-18 16:19:41 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,28 +1,35 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    ArrayList<String> strings = new ArrayList<>();
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Luis Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
+        if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                strings.add(parameters[1] + "");
+                return String.format("%s added to list", parameters[1]);
             }
-            return "404 Not Found!";
         }
+        else if (url.getPath().contains("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            
+            if (parameters[0].equals("s")) {
+                String searchTerm = parameters[1] + "";
+                String returnString = "";
+                for (int i = 0; i < strings.size(); i++) {
+                    if (strings.get(i).contains(searchTerm)) {
+                        returnString += strings.get(i) + ", ";
+                    }
+                }
+                return returnString;
+            }
+        }
+        return "404 Not Found!";
     }
 }
 
