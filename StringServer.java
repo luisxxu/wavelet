25d81af1facdc1db054c3170457448cda54e7b6a
luisxxu <lxxu@ucsd.edu>
2023-02-13 21:16:06 -0800

    a

---
+++ StringServer.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> strings = new ArrayList<>();
+    String returnString = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                returnString += parameters[1] + "\n";
+            }
+            System.out.println(returnString);
+            return String.format(returnString);
+        }
+        return "404 Not Found!";
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
